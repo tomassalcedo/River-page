765970ad8d72b28cf3ddb95cb8dca0d7231c8f31
Tomas Salcedo <tomas.river12[200~>
2024-05-01 00:38:48 -0300

    first test

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// document.querySelector('.menu-icon').addEventListener('click', function() {
+//     document.querySelector('.menu').classList.toggle('active'); // Agregar o quitar la clase 'active' al menú
+// });
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('.menu-icon').addEventListener('click', function() {
+//         document.querySelector('.menu').classList.toggle('active');
+//     });
+// });
+
+// document.querySelector('.menu-icon').addEventListener('click', function() {
+//     document.querySelector('.menu').classList.toggle('active'); // Alternar la clase 'active' en el menú
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.menu-icon').addEventListener('click', function() {
+        document.querySelector('.menu').classList.toggle('active');
+    });
+});
+
+
+
+
+
+
+
+
+
+
